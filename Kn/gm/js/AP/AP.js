5c3d3c3046b44a72ef01d1770131a63ba50d0b51
Knives77 <pepinilloinquisidor@gmail.com>
2023-05-01 22:58:59 -0600

    Cuatro

--- Kn/gm/js/AP/AP.js
+++ Kn/gm/js/AP/AP.js
@@ -1,41 +1,47 @@
-// //Apuesta y presupuesto
-// let money = 500;
-// let apuesta = 100;
-// let numero = 7;
+//Apuesta y presupuesto
 
-// //variables random
-// let a, b, z;
+let vidas = 3;
+let numero = 7;
+let money;
+let apuesta;
 
-// //vidas
+let juego = () => {
+  a = parseInt(Math.random() * 10 + 1);
+  b = parseInt(Math.random() * 10 + 1);
+  c = parseInt(Math.random() * 10 + 1);
 
-// let vidas = 3;
+  console.log(a, b, c);
 
-// a = parseInt(Math.random() * 10 + 1);
-// b = parseInt(Math.random() * 10 + 1);
-// c = parseInt(Math.random() * 10 + 1);
+   if (money === 0) {
+     alert("lose");
+    } else {
+      if (money >= apuesta) {
+        if (a === numero || b === numero || c === numero) {
+          alert("Ganaste "+(apuesta + apuesta));
+          apuesta += apuesta;
+          document.getElementById("aña1").innerHTML = `${(money + apuesta)}`
+          console.log(apuesta);
+          console.log(vidas);
+      } else {
+        alert("Perdiste :c "+(apuesta));
+        money -= apuesta;
+        console.log(money);
+        document.getElementById("aña1").innerHTML = `${money}`
+        vidas--;
+        console.log(vidas);
+      }
+    } else {
+    alert("Fondo insuficiente");
+    }
+  }
+}
 
-// console.log(a, b, c);
+document.getElementById("ff").onclick = function fondos() {
+	const a = parseInt(document.getElementById("dinero").value);
+	return document.getElementById("aña1").innerHTML += ` ${a}`, document.getElementById("ff").disabled = true, money = a, document.getElementById("dinero").value = "";
+}
 
-// if (money === 0) {
-//   console.log("lose");
-// } else {
-//   if (money > apuesta) {
-//     if (a === numero || b === numero || c === numero) {
-//         console.log("Ganaste :)");
-//         console.log("Ganaste ", (apuesta + apuesta));
-//         apuesta += apuesta;
-//         console.log(apuesta);
-//         console.log(vidas);
-
-//     } else {
-//         console.log("Perdiste :(");
-//         console.log("Perdiste ", (money - apuesta));
-//         money -= apuesta;
-//         console.log(money);
-//         vidas--;
-//         console.log(vidas);
-//     }
-//   } else {
-//     console.log("Fondo insuficiente");
-//   }
-// }
+document.getElementById("ap").onclick = function fondos() {
+	const b = parseInt(document.getElementById("apuesta").value);
+	return document.getElementById("aña2").innerHTML = `${b}`, apuesta = b/*, document.getElementById("ap").disabled = true,*/, juego();
+}
