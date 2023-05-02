c7e0f2fbb90ebfda80dfac621c1f85d06947e503
Knives77 <pepinilloinquisidor@gmail.com>
2023-05-02 08:38:18 -0600

    "Cinco"

--- Kn/gm/js/AP/AP.js
+++ Kn/gm/js/AP/AP.js
@@ -1,47 +1,67 @@
 //Apuesta y presupuesto
 
-let vidas = 3;
+let bonus;
 let numero = 7;
 let money;
 let apuesta;
 
-let juego = () => {
-  a = parseInt(Math.random() * 10 + 1);
-  b = parseInt(Math.random() * 10 + 1);
-  c = parseInt(Math.random() * 10 + 1);
+const juego = () => {
+  a = parseInt(Math.random() * 20 + 1);
+  b = parseInt(Math.random() * 20 + 1);
+  c = parseInt(Math.random() * 20 + 1);
 
-  console.log(a, b, c);
 
-   if (money === 0) {
-     alert("lose");
-    } else {
-      if (money >= apuesta) {
-        if (a === numero || b === numero || c === numero) {
-          alert("Ganaste "+(apuesta + apuesta));
-          apuesta += apuesta;
-          document.getElementById("aña1").innerHTML = `${(money + apuesta)}`
-          console.log(apuesta);
-          console.log(vidas);
+  fill("#d9c3f7");
+  textSize(44);
+  text("a: " + a, 30, 50);
+  text("b: " + b, 150, 50);
+  text("c: " + c, 270, 50);
+
+
+  if (money === 0) {
+    alert("lose");
+  } else {
+    if (money >= apuesta) {
+      if (a === numero || b === numero || c === numero) {
+        alert("Ganaste " + (apuesta + apuesta));
+        apuesta += apuesta;
+        document.getElementById("aña1").innerHTML = `${money + apuesta}`;
+        console.log(apuesta);
       } else {
-        alert("Perdiste :c "+(apuesta));
+        alert("Perdiste :c " + apuesta);
         money -= apuesta;
         console.log(money);
-        document.getElementById("aña1").innerHTML = `${money}`
-        vidas--;
-        console.log(vidas);
+        document.getElementById("aña1").innerHTML = `${money}`;
       }
     } else {
-    alert("Fondo insuficiente");
+      alert("Fondo insuficiente");
     }
   }
+};
+
+function setup() {
+  const canvas = createCanvas(1000, 300);
+  canvas.parent("#cont");
+  background("black");
 }
 
-document.getElementById("ff").onclick = function fondos() {
-	const a = parseInt(document.getElementById("dinero").value);
-	return document.getElementById("aña1").innerHTML += ` ${a}`, document.getElementById("ff").disabled = true, money = a, document.getElementById("dinero").value = "";
-}
+function draw() {
+  document.getElementById("ff").onclick = function fondos() {
+    const a = parseInt(document.getElementById("dinero").value);
+    return (
+      (document.getElementById("aña1").innerHTML += ` ${a}`),
+      (document.getElementById("ff").disabled = true),
+      (money = a),
+      (document.getElementById("dinero").value = "")
+    );
+  };
 
-document.getElementById("ap").onclick = function fondos() {
-	const b = parseInt(document.getElementById("apuesta").value);
-	return document.getElementById("aña2").innerHTML = `${b}`, apuesta = b/*, document.getElementById("ap").disabled = true,*/, juego();
+  document.getElementById("ap").onclick = function fondos() {
+    const b = parseInt(document.getElementById("apuesta").value);
+    return (
+      (document.getElementById("aña2").innerHTML = `${b}`),
+      (apuesta = b) /*, document.getElementById("ap").disabled = true,*/, clear(),
+      juego()
+    );
+  };
 }
